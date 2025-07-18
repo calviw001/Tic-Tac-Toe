3018964aeced0512f42156d623652fe7082ccafd
calviw001 <cowvynyia@gmail.com>
2025-07-18 15:23:03 -0700

    Updated print_board()
    I changed the spacing.

--- engine.py
+++ engine.py
@@ -13,12 +13,15 @@ def create_board() -> list:
 def print_board(board: list) -> None:
     print("   0   1   2")
     for row_num in range(3):
-        print(f"{row_num} ", end=" ")
+        print(f"{row_num}", end=" ")
         for column_num in range(3):
             cell = board[row_num][column_num]
-            print(f"{cell if cell else '-'} ", end="")
+            print(f"{cell if cell else ' - '}", end="")
             if column_num < 2:
-                print("| ", end="")
+                print("|", end="")
         print()
         if row_num < 2:
             print("   ---------")
+
+
+# print_board(create_board())
