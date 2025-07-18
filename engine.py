75b55016cca330efe39577eee37abbe72111472b
calviw001 <cowvynyia@gmail.com>
2025-07-18 14:53:54 -0700

    Created print_board()
    This function will print out the provide game board in a readable format.

--- engine.py
+++ engine.py
@@ -8,3 +8,17 @@ def create_board() -> list:
         [None, None, None]
     ]
     return board
+
+
+def print_board(board: list) -> None:
+    print("   0   1   2")
+    for row_num in range(3):
+        print(f"{row_num} ", end=" ")
+        for column_num in range(3):
+            cell = board[row_num][column_num]
+            print(f"{cell if cell else '-'} ", end="")
+            if column_num < 2:
+                print("| ", end="")
+        print()
+        if row_num < 2:
+            print("   ---------")
