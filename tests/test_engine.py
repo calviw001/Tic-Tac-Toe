752e779e709a91d103bd6c7a23001790cf187f1a
calviw001 <cowvynyia@gmail.com>
2025-07-19 14:56:52 -0700

    Created test_check_winner() and added more comments
    This function tests whether my previous function is able to detect whether a board contains a winning piece or not. Additionally, I added some more comments to my previous tests.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -34,18 +34,18 @@ class TestGameEngine(unittest.TestCase):
     def test_is_move_valid(self):
         # Test move validation
         board = engine.create_board()
-        
+        # Valid move
         self.assertTrue(engine.is_move_valid(('0', '0'), board))
         self.assertTrue(engine.is_move_valid(('1', '2'), board))
-        
+        # Out of bounds move
         self.assertFalse(engine.is_move_valid(('-1', '0'), board))
         self.assertFalse(engine.is_move_valid(('4', '1'), board))
-        
+        # Move contains non numbers
         self.assertFalse(engine.is_move_valid(('a', '1'), board))
-        
+        # Move tries to take up occupied space
         board[1][1] = 'X'
         self.assertFalse(engine.is_move_valid(('1', '1'), board))
-        
+        # Move tuple is not length 2
         self.assertFalse(engine.is_move_valid(('1'), board))
         self.assertFalse(engine.is_move_valid(('1', '2', '3'), board))
 
@@ -61,20 +61,54 @@ class TestGameEngine(unittest.TestCase):
 
     def test_is_board_full(self):
         # Test checking of a board is full or not
+        # Not full
         board = [[None, None, None],
                  [None, None, None],
                  [None, None, None]]
         self.assertFalse(engine.is_board_full(board))
-    
         board = [['X', None, 'O'],
                  [None, 'X', None],
                  ['O', None, 'X']]
         self.assertFalse(engine.is_board_full(board))
-    
+        # Is full
         board = [['X', 'O', 'X'],
                  ['O', 'X', 'O'],
                  ['O', 'X', 'O']]
         self.assertTrue(engine.is_board_full(board))
+
+
+    def test_check_winner(self):
+        # Test checking of a winner or not
+        # Rows
+        board = [['X', 'X', 'X'],
+                 ['O', None, 'O'],
+                 ['O', 'X', None]]
+        self.assertEqual(engine.check_winner(board), 'X')
+        # Columns
+        board = [['O', None, 'X'],
+                 ['O', 'X', 'O'],
+                 ['O', 'X', None]]
+        self.assertEqual(engine.check_winner(board), 'O')
+        # Top-left to bottom-right
+        board = [['X', 'O', 'X'],
+                 ['O', 'X', 'O'],
+                 ['O', 'X', 'X']]
+        self.assertEqual(engine.check_winner(board), 'X')
+        # Top-right to bottom-left
+        board = [['X', 'O', 'O'],
+                 ['O', 'O', 'X'],
+                 ['O', 'X', None]]
+        self.assertEqual(engine.check_winner(board), 'O')
+        # Partially filled board with no winner
+        board = [['X', 'O', 'X'],
+                 ['O', 'X', 'O'],
+                 ['O', 'X', None]]
+        self.assertIsNone(engine.check_winner(board))
+        # Full board with no winner
+        board = [['X', 'O', 'X'],
+                 ['X', 'X', 'O'],
+                 ['O', 'X', 'O']]
+        self.assertIsNone(engine.check_winner(board))
 
 
 if __name__ == '__main__':
