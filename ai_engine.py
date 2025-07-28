e8b2453a9fcdf6357693a929b5754a00dbd7c22f
calviw001 <cowvynyia@gmail.com>
2025-07-28 15:12:31 -0700

    Created check_two_in_a_row()
    This function checks whether one of the pieces is only one more piece away from victory.

--- ai_engine.py
+++ ai_engine.py
@@ -31,6 +31,40 @@ def make_board_copy(board: list) -> list:
     return board_copy
 
 
+def check_two_in_a_row(board: list) -> str:
+    ai_winning = False
+    player_winning = False
+    # Check rows
+    for each_row in board:
+        if each_row.count('O') == 2 and None in each_row:
+            ai_winning = True
+        elif each_row.count('X') == 2 and None in each_row:
+            player_winning = True
+    # Check columns
+    for column_num in range(3):
+        each_column = [board[0][column_num], board[1][column_num], board[2][column_num]]
+        if each_column.count('O') == 2 and None in each_column:
+            ai_winning = True
+        elif each_column.count('X') == 2 and None in each_column:
+            player_winning = True
+    # Check diagonals
+    diagonal1 = [board[0][0], board[1][1], board[2][2]]
+    diagonal2 = [board[0][2], board[1][1], board[2][0]]
+    if (diagonal1.count('O') == 2 and None in diagonal1) or (diagonal2.count('O') == 2 and None in diagonal2):
+        ai_winning = True
+    elif (diagonal1.count('X') == 2 and None in diagonal1) or (diagonal2.count('X') == 2 and None in diagonal2):
+        player_winning = True
+    # Format result
+    if ai_winning and player_winning:
+        return "Both"
+    elif ai_winning and not player_winning:
+        return "AI"
+    elif not ai_winning and player_winning:
+        return "Player"
+    else:
+        return "Neither"
+
+
 def minimax_score(board: list, current_player_piece: str) -> int:
     # player_piece = 'X'
     # ai_piece = 'O'
@@ -67,8 +101,11 @@ def get_move_ai(board: list) -> tuple:
     all_moves = get_all_available_moves(board)
     for each_move in all_moves:
         new_board = make_board_copy(board)
-        engine.update_board(each_move, new_board, ai_piece)
+        # engine.update_board(each_move, new_board, ai_piece)
+        # engine.print_board(new_board)
         current_move_score = minimax_score(new_board, ai_piece)
+        # print(current_move_score)
+        # print()
         if current_move_score > move_score:
             move = each_move
             move_score = current_move_score
